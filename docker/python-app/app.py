7b4846502d3ef12f59e113bf0b343b831069b012
Alexander Yaroshenko <zava11955@gmail.com>
2021-03-30 00:13:18 +0300

    fixed Python app and added Dockerfile

--- docker/python-app/app.py
+++ docker/python-app/app.py
@@ -3,12 +3,16 @@ import os
 import requests
 
 app = Flask(__name__)
-
+app_host = os.environ['APP_HOST']
 
 @app.route('/')
 def hello():
-    my_env = os.environ['USER']
+    try:
+        my_env = os.environ['HOSTNAME']
+    except KeyError:
+        my_env = os.environ['USER']
+
     return my_env
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=app_host)

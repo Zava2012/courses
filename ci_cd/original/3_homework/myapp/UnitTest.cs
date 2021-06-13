6f4df34480f3664d3c30ae5c8f6360f6389ffeed
Alexander Yaroshenko <zava11955@gmail.com>
2021-06-13 13:33:58 +0300

    added new information

---
+++ ci_cd/original/3_homework/myapp/UnitTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace aspnetapp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
